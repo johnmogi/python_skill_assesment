9496d16b1fb2d5811d42895524436ffcb7e6ccb4
johnmogi <anguru@gmail.com>
2021-08-25 18:53:42 +0300

    1

---
+++ 1.py
@@ -0,0 +1,21 @@
+chinese_speakers = 1107
+chinese_web_share = 0.017
+
+english_speakers = 1121
+english_web_share = 0.539
+
+russian_speakers = 264.3
+russian_web_share = 0.061	
+
+print("--- Chinese ---")
+print("Share of speakers:",  chinese_speakers, "%" )
+print("Share of websites in the language:", chinese_web_share, "%" )
+print("Web popularity index:", chinese_speakers / chinese_web_share)
+print("--- English ---")
+print("Share of speakers:",  english_speakers, "%" )
+print("Share of websites in the language:", english_web_share, "%" )
+print("Web popularity index:", english_speakers / english_web_share )
+print("--- Russian ---")
+print("Share of speakers:",  russian_speakers, "%" )
+print("Share of websites in the language:", russian_web_share, "%" )
+print("Web popularity index:", russian_speakers / russian_web_share )
